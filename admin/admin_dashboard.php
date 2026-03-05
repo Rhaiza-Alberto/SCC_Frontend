f29aaf676a117c9900c1761b4a683758f14af282
Rhaiza726 <ae202403480@wmsu.edu.ph>
2026-03-05 22:00:41 +0800

    spelling issue

--- admin/admin_dashboard.php
+++ admin/admin_dashboard.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once __DIR__ . '/../database.php';
+require_once __DIR__ . '../database.php';
 require_once __DIR__ . '/../functions.php';
 
 // Check if user is logged in

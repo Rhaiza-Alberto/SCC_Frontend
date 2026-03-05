e6d298d2d6fcce200ab5004f5c2d0ae53eb07a25
flor-glitch-ai <ae202404182@wmsu.edu.ph>
2026-03-06 00:34:35 +0800

    Update process_login.php

--- process_login.php
+++ process_login.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once _DIR_ . '/database.php';
-require_once _DIR_ . '/functions.php';
+require_once  __DIR__ . '/database.php';
+require_once __DIR__ . '/functions.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email    = trim($_POST['email'] ?? '');
